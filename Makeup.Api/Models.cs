22ed4531d694ea242e2138eca3c55d1cce340f55
xnetvoya <xnetvoyax@gmail.com>
2025-09-04 00:00:37 +0200

    created models.cs

---
+++ Makeup.Api/Models.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Makeup.Domain.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+        public ICollection<Order> Orders { get; set; } = new List<Order>();
+        public ICollection<Review> Reviews { get; set; } = new List<Review>();
+    }
+    public class Category
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+    public class Brand
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public ICollection<Product> Products { get; set; } = new List<Product>();
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+        public ICollection<ProductImage> Images { get; set; } = new List<ProductImage>();
+        public ICollection<Review> Reviews { get; set; } = new List<Review>();
+        public ICollection<OrderItem> OrderItems { get; set; } = new List<OrderItem>();
+    }
+    public class ProductImage
+    {
+        public int Id { get; set; }
+        public required string Url { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public ICollection<OrderItem> Items { get; set; } = new List<OrderItem>();
+    }
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+    public class Review
+    {
+        public int Id { get; set; }
+        public required string Content { get; set; }
+        public int Rating { get; set; } 
+        public int UserId { get; set; }
+        public User User { get; set; }
+        public int ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
